71f08c94cd29e48ccf534f78b7d38eb514765bf6
mortalla23 <148428892+mortalla23@users.noreply.github.com>
2023-11-08 15:19:49 +0100

    Add files via upload

---
+++ presen.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <title>Document</title>
+</head>
+<body>
+        
+    <nav class="cc-navbar navbar navbar-expand-lg position-fixed navbar-dark w-100">
+        <div class="container">
+          <a class="navbar-brand test-uppercase fw-bolder mx-4 py-3" href="#">GameSphère</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item pe-2">
+                <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+              </li>
+              
+              <li class="nav-item pe-2">
+                <a class="nav-link" href ="presen.php">Presentation</a>
+              </li>
+              <li class="nav-item pe-2">
+                <a class="nav-link" href ="inscrip.php">Devenir membre</a>
+              </li>
+              <li class="nav-item pe-2">
+                <a class="btn btn-order rounded-0" href ="conne.php">Connection</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <section class="presen d-flex justify-content-center align-items-center pt-5">
+        <div class="container  ">
+            <div class ="row ">
+                    <h1 class="text-center redressed">Bienvenue</h1>
+                     <h2 class="text-center redressed " >
+                        GameSphère est votre portail en ligne dédié aux amateurs de jeux de plateau, offrant une expérience de jeu immersive et divertissante. Plongez dans l'univers des jeux de plateau classiques tels que Monopoly, jeu de carte, ludo, tous réunis sur une plateforme conviviale.</br><u>Présentation</u>
+                     </h2>  
+                     
+                        <h4 class="text-center redressed">1.<u>Une Collection Étendue de Jeux de Plateau </u>  :</h4>
+                        <p class="text-center"> Que vous soyez un vétéran des jeux de société ou un débutant enthousiaste, notre collection diversifiée comprend une gamme de jeux de plateau populaires. Choisissez parmi les classiques intemporels, les derniers jeux tendance et les perles méconnues.</p>
+                        
+                        <h5 class="text-center redressed">2. <u>Parties en Ligne et Multijoueurs</u></h5>
+                        <p class="text-center">Jouez avec vos amis, votre famille ou des joueurs du monde entier grâce à nos fonctionnalités multijoueurs. Rejoignez des tournois passionnants pour montrer vos compétences.</p>
+                    
+                        <h8 class="text-center redressed">3. <u>Communauté Engagée</u> </h8>
+                        <p class="text-center ">Rejoignez notre communauté passionnée de joueurs de plateau. Participez à des discussions, partagez des stratégies et organisez des tournois. Faites de nouveaux amis tout en partageant votre amour des jeux de société.</p>
+                        
+                        <h9 class="text-center redressed">5.<u>Jeux sur Tous les Appareils</u>  </h9>
+                        <p class="text-center">Profitez de l'excitation des jeux de plateau sur n'importe quel appareil - que ce soit sur votre ordinateur, votre tablette ou votre smartphone. Jouez à tout moment, n'importe où.</p>
+                        
+                        <h10 class="text-center redressed">6.<u>Pourquoi Choisir GameSphère?</u> </h10>
+                        <p class="text-center"> GameSphère se distingue largement des autres. Nous mettons l'accent sur l'expérience utilisateur, offrant un site sécurisé, convivial et accessible à tous les âges et niveaux de compétence. Notre équipe passionnée travaille constamment pour élargir notre collection de jeux, garantissant ainsi que chaque visite sur GameSphère soit une aventure ludique inoubliable.</p>
+                    
+                        <p class="text-center">Préparez-vous à plonger dans un monde de compétition amicale, d'intrigues stratégiques et d'innombrables heures de divertissement. Rejoignez-nous dès aujourd'hui sur GameSphère et préparez-vous à jouer, à rire et à gagner!
+                        </p>
+                      
+                    
+                
+            </div>
+        </div>
+  </section>
+  <section class="footer py-5 d-flex justify-content-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex align-items-center">
+                <p class="text-white">&copy; 2023 Rouen | All rights reserved.</p>
+            </div>
+            <div class="col-6">
+                <ul class="nav text-center">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Jeux</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+                
+        </div>
+       
+    </div>
+  </section>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
